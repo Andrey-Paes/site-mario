1204fb9d11be22c4353f34d50f5769dd606c6fc8
Andrey Paes <63912377+Andreivison@users.noreply.github.com>
2025-03-26 11:50:47 -0400

    adicionado a funcao do JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("funcionou")
+const form = document.querySelector(".formulario-fale-conosco")
+
+function cliqueiNoBotao(){
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    
+}
+
+
+/*form.slyte.left = "50%"
+    form.slyte.transform = "translateX(-50%)"*/

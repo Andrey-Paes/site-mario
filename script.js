a7aafc1708e1f003765c5dbc49adb6dfb0219470
Andrey Paes <63912377+Andreivison@users.noreply.github.com>
2025-03-31 12:39:55 -0400

    Alterações no JS criado a funçao esconderForm

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const form = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
 
-function cliqueiNoBotao(){
+function mostrarForm(){
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
     
+    
 }
-
+function esconderForm(){
+    form.style.left = "-130px"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "hidden"
+    
+}
 
 /*form.slyte.left = "50%"
     form.slyte.transform = "translateX(-50%)"*/

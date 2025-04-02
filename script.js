e8d7c4f1527296a0686405da91e38324671dad92
Andrey Paes <63912377+Andreivison@users.noreply.github.com>
2025-04-02 13:07:03 -0400

    Alterações no js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function mostrarForm(){
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
-    
+
     
 }
 function esconderForm(){
